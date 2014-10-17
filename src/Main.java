07462e4623e289878e22e431bafac5ba5edd9ee4
Spencer York <spencery@me.com>
2014-10-18 02:47:21 +1100

    Updated Formatting and Optimised imports.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.*;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,12 +30,11 @@ public class Main {
 
     private static void printData() {
         System.out.println("Printing Data now...\n");
-        for (int i = 0; i < ranked.size(); i++)
-        {
+        for (int i = 0; i < ranked.size(); i++) {
             System.out.println("--------");
             System.out.print("Title: " + linktext.get(i) + "\n");
             System.out.print("Link: " + "[" + link.get(i) + "]" + "\n");
-            System.out.print("Rank: " + ranked.get(i)+ "\n");
+            System.out.print("Rank: " + ranked.get(i) + "\n");
         }
         linktext.clear();
         link.clear();
@@ -54,14 +55,11 @@ public class Main {
             for (Element titles : title) {
                 /**This is controlling empty titles**/
                 if (titles.attr("href") != "") {
-                    if (titles.attr("href").contains("/r/"))
-                    {
+                    if (titles.attr("href").contains("/r/")) {
                         String tempLink = titles.attr("href");
                         link.add("http://www.reddit.com" + tempLink);
-                    }
-                    else
-                    {
-                    link.add(titles.attr("href"));
+                    } else {
+                        link.add(titles.attr("href"));
                     }
                     linktext.add(titles.text());
                 }
@@ -84,7 +82,7 @@ public class Main {
         }
     }
 
-    public static void choice () {
+    public static void choice() {
         System.out.println("");
         System.out.println("--------");
         System.out.println("");
@@ -101,8 +99,7 @@ public class Main {
             System.out.println("IO error trying to read your choice!");
             System.exit(1);
         }
-        if (Integer.parseInt(selection) == 1)
-        {
+        if (Integer.parseInt(selection) == 1) {
             System.out.println();
             System.out.println(" -- Reddit Scrape! -- ");
             try {
@@ -114,8 +111,7 @@ public class Main {
             getRanks();
             printData();
         }
-        if (Integer.parseInt(selection) == 2)
-        {
+        if (Integer.parseInt(selection) == 2) {
             System.out.println("Subreddit?");
             BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
             String subreddit = null;
@@ -128,8 +124,7 @@ public class Main {
             String reddit = "http://www.reddit.com/r/";
             URL = null;
             URL = reddit + subreddit;
-            if (subreddit.isEmpty())
-            {
+            if (subreddit.isEmpty()) {
                 URL = "http://www.reddit.com";
             }
             linktext.clear();
@@ -147,13 +142,9 @@ public class Main {
             printData();
         }
 
-        if (Integer.parseInt(selection) == 3)
-        {
-           System.exit(0);
-        }
-
-        else
-        {
+        if (Integer.parseInt(selection) == 3) {
+            System.exit(0);
+        } else {
             System.out.println("Enter a listed option.");
             choice();
         }
